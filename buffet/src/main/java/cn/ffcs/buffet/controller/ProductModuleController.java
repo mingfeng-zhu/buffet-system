a37a8360ad0f8a0c8afd4e5b442f7e1c5e7d0396
18950180450 <chenzhen_c@163.com>
2020-08-21 22:29:23 +0800

    商品接口加入字段显示购物车加入该商品的数量

--- buffet/src/main/java/cn/ffcs/buffet/controller/ProductModuleController.java
+++ buffet/src/main/java/cn/ffcs/buffet/controller/ProductModuleController.java
@@ -1,8 +1,11 @@
 package cn.ffcs.buffet.controller;
 
+import cn.ffcs.buffet.common.annotation.PassToken;
 import cn.ffcs.buffet.common.dto.Page;
 import cn.ffcs.buffet.common.dto.Result;
+import cn.ffcs.buffet.common.util.TokenUtil;
 import cn.ffcs.buffet.model.dto.*;
+import cn.ffcs.buffet.model.po.UserPO;
 import cn.ffcs.buffet.service.ProductModuleService;
 import cn.ffcs.buffet.service.ShopCartService;
 import com.github.pagehelper.PageInfo;
@@ -12,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -38,59 +42,80 @@ public class ProductModuleController {
         return Result.success(productCategoryDTOList);
     }
 
+    @PassToken
     @ApiOperation(value = "根据商品分类id获取商品列表")
     @GetMapping(path = "/getProductListByProductCategoryId")
-    public Result getProductListByProductCategoryId(Page<ProductDTO> page, Integer productCategoryId) {
-        Integer userId = 1;
+    public Result getProductListByProductCategoryId(Page<ProductDTO> page, Integer productCategoryId, HttpServletRequest request) {
+        Object user = request.getSession().getAttribute("user");
+        UserDTO userIdAndUserTelOfToken = TokenUtil.getUserIdAndUserTelOfToken();
 
         PageInfo<ProductDTO> productDTOList = productModuleService.selectProductListByProductCategoryId(page, productCategoryId);
         if(productDTOList.getTotal() > 0){
 
             List<ProductDTO> list = productDTOList.getList();
-            List<ShopCartDetailDTO> shopCartDetailDTOList = (List<ShopCartDetailDTO>)shopCartService.listShopCartByUserId(userId).getData();
 
-            for (int i = 0; i < list.size(); i++) {
-                for (int j = 0; j < shopCartDetailDTOList.size(); j++) {
-                    if(list.get(i).getProductId() == shopCartDetailDTOList.get(j).getProductSpecificationDTO().getProductId()){
-                        if(list.get(i).getProductNumOfCart() == null){
-                            list.get(i).setProductNumOfCart(0);
-                        }
-                        list.get(i).setProductNumOfCart(list.get(i).getProductNumOfCart() + shopCartDetailDTOList.get(j).getShopCart().getGoodCount());
-                    }
-                }
+            if (userIdAndUserTelOfToken != null){   //能获取到用户信息，就设置商品加入购物车的数量
+                Integer userId = userIdAndUserTelOfToken.getUserId();
+                List<ShopCartDetailDTO> shopCartDetailDTOList = (List<ShopCartDetailDTO>)shopCartService.listShopCartByUserId(userId).getData();
+                list = setProductNumOfCart(list, shopCartDetailDTOList);
             }
 
-            page.setList(productDTOList.getList());
+            page.setList(list);
             page.setTotal(productDTOList.getTotal());
             return Result.success(page);
         }
         return Result.success();
     }
 
+    @PassToken
     @ApiOperation(value = "根据商品名称查询商品")
     @GetMapping(path = "/getProductListByProductName")
-    public Result getProductListByProductName(Page<ProductDTO> page, String productName){
+    public Result getProductListByProductName(Page<ProductDTO> page, String productName, HttpServletRequest request){
+        Object user = request.getSession().getAttribute("user");
+        UserDTO userIdAndUserTelOfToken = TokenUtil.getUserIdAndUserTelOfToken();
+
         PageInfo<ProductDTO> productDTOList = productModuleService.selectProductListByProductName(page, productName);
         if(productDTOList.getTotal() > 0){
-            page.setList(productDTOList.getList());
+            List<ProductDTO> list = productDTOList.getList();
+
+            if (userIdAndUserTelOfToken != null){   //能获取到用户信息，就设置商品加入购物车的数量
+                Integer userId = userIdAndUserTelOfToken.getUserId();
+                List<ShopCartDetailDTO> shopCartDetailDTOList = (List<ShopCartDetailDTO>)shopCartService.listShopCartByUserId(userId).getData();
+                list = setProductNumOfCart(list, shopCartDetailDTOList);
+            }
+
+            page.setList(list);
             page.setTotal(productDTOList.getTotal());
             return Result.success(page);
         }
         return Result.success();
     }
 
+    @PassToken
     @ApiOperation(value = "获取全部商品")
     @GetMapping(path = "/getAllProductList")
-    public Result getAllProductList(Page<ProductDTO> page){
+    public Result getAllProductList(Page<ProductDTO> page, HttpServletRequest request){
+        Object user = request.getSession().getAttribute("user");
+        UserDTO userIdAndUserTelOfToken = TokenUtil.getUserIdAndUserTelOfToken();
+
         PageInfo<ProductDTO> productDTOList = productModuleService.selectAllProductList(page);
         if(productDTOList.getTotal() > 0){
-            page.setList(productDTOList.getList());
+            List<ProductDTO> list = productDTOList.getList();
+
+            if (userIdAndUserTelOfToken != null){   //能获取到用户信息，就设置商品加入购物车的数量
+                Integer userId = userIdAndUserTelOfToken.getUserId();
+                List<ShopCartDetailDTO> shopCartDetailDTOList = (List<ShopCartDetailDTO>)shopCartService.listShopCartByUserId(userId).getData();
+                list = setProductNumOfCart(list, shopCartDetailDTOList);
+            }
+
+            page.setList(list);
             page.setTotal(productDTOList.getTotal());
             return Result.success(page);
         }
         return Result.success();
     }
 
+    @PassToken
     @ApiOperation(value = "根据商品id获取商品属性及其属性值")
     @GetMapping(path = "/getProductAttributeListByProductId/{productId}")
     public Result getProductAttributeListByProductId(@PathVariable Integer productId){
@@ -98,6 +123,7 @@ public class ProductModuleController {
         return Result.success(productAttributeDTOList);
     }
 
+    @PassToken
     @ApiOperation(value = "根据商品id和商品规格获取该商品的详细信息")
     @GetMapping(path = "/getSpecificationByProductIdAndSpecification")
     public Result getSpecificationByProductIdAndSpecification(@RequestParam(name = "productId") Integer productId, @RequestParam(name = "productSpecification") String productSpecification){
@@ -105,10 +131,31 @@ public class ProductModuleController {
         return Result.success(productSpecificationDTO);
     }
 
+    @PassToken
     @ApiOperation(value = "根据商品id获取商品详细信息以及商品评价")
     @GetMapping(path = "/getProductDetailAndCommentList/{productId}")
     public Result getProductDetailAndCommentList(@PathVariable Integer productId){
         List<ProductDTO> productDTOList = productModuleService.selectProductDetailAndCommentList(productId);
         return Result.success(productDTOList);
+    }
+
+    /**
+     * 设置商品加入购物车的数量
+     * @param list
+     * @param shopCartDetailDTOList
+     * @return
+     */
+    public List<ProductDTO> setProductNumOfCart(List<ProductDTO> list, List<ShopCartDetailDTO> shopCartDetailDTOList){
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < shopCartDetailDTOList.size(); j++) {
+                if(list.get(i).getProductId() == shopCartDetailDTOList.get(j).getProductSpecificationDTO().getProductId()){
+                    if(list.get(i).getProductNumOfCart() == null){
+                        list.get(i).setProductNumOfCart(0);
+                    }
+                    list.get(i).setProductNumOfCart(list.get(i).getProductNumOfCart() + shopCartDetailDTOList.get(j).getShopCart().getGoodCount());
+                }
+            }
+        }
+        return list;
     }
 }
