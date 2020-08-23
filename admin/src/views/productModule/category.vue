49650f7e8765c6b4691c2df82876d307ed06cf13
18950180450 <chenzhen_c@163.com>
2020-08-23 16:18:08 +0800

    新增后台管理商品模块部分接口、后台管理商品模块分类管理界面

---
+++ admin/src/views/productModule/category.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="app-container">
+    <el-button type="primary" size="small" style="margin: 10px 0" @click="showDialog(type = 'add')">新增</el-button>
+    <el-table :data="categoryList" border style="width: 100%">
+      <el-table-column fixed prop="productCategoryId" label="分类ID" />
+      <el-table-column fixed prop="categoryName" label="分类名" />
+      <el-table-column fixed prop="categoryDesc" label="分类描述" />
+      <el-table-column fixed prop="createTime" label="创建时间" />
+      <el-table-column fixed prop="updateTime" label="更新时间" />
+      <el-table-column fixed="right" label="操作">
+        <template slot-scope="scope">
+          <el-button
+            type="primary"
+            size="small"
+            @click="showDialog(type = 'update', scope.$index)"
+          >修改</el-button>
+          <el-popconfirm title="确定要删除吗？" @onConfirm="deleteCategory(scope.$index)">
+            <el-button slot="reference" type="danger" size="small">删除</el-button>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-dialog title="创建商品分类" :visible.sync="dialogFormVisible" width="30%">
+      <el-form ref="ruleForm" :model="category" :rules="rules">
+        <el-form-item label="分类名称" prop="categoryName">
+          <el-input v-model="category.categoryName" />
+        </el-form-item>
+        <el-form-item label="分类描述" prop="categoryDesc">
+          <el-input v-model="category.categoryDesc" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="float: right" @click="addCategory('ruleForm')">确 定</el-button>
+          <el-button style="float: right" @click="dialogFormVisible = false">取 消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      categoryList: [
+        {
+          productCategoryId: 1,
+          categoryName: '分类1',
+          categoryDesc: '分类描述1',
+          createTime: '2019-09-18 02:54:35',
+          updateTime: '2019-09-18 02:54:35'
+        },
+        {
+          productCategoryId: 2,
+          categoryName: '分类2',
+          categoryDesc: '分类描述2',
+          createTime: '2019-09-18 02:54:35',
+          updateTime: '2019-09-18 02:54:35'
+        }
+      ],
+      rules: {
+        categoryName: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' },
+          { min: 2, max: 20, message: '长度在2~10个字符', trigger: 'blur' }
+        ],
+        categoryDesc: [
+          { required: true, message: '请输入分类描述', trriger: 'blur' },
+          { max: 100, message: '不能超过100个字符', trriger: 'blur' }
+        ]
+      },
+      category: {
+        productCategoryId: null,
+        categoryName: null,
+        categoryDesc: null,
+        createTime: null,
+        updateTime: null
+
+      },
+      visible: false,
+      dialogFormVisible: false,
+      updateIndex: null
+    }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+      console.log('init')
+    },
+    addCategory(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    updateCategory() {
+      this.dialogFormVisible = false
+    },
+    deleteCategory(index) {
+      console.log('shanchu' + this.categoryList[index].productCategoryId)
+      this.visible = false
+    },
+    showDialog(type, index) {
+      if (type === 'add') {
+        this.category.productCategoryId = null
+        this.category.categoryName = null
+        this.category.categoryDesc = null
+        this.category.createTime = null
+        this.category.updateTime = null
+      }
+      if (type === 'update') {
+        this.category = JSON.parse(JSON.stringify(this.categoryList[index]))
+      }
+      this.dialogFormVisible = true
+      // console.log('type: ' + type + ', index: ' + index)
+    }
+  }
+}
+</script>
---
+++ buffet/src/main/java/cn/ffcs/buffet/controller/admin/ProductManageController.java
@@ -0,0 +1,72 @@
+package cn.ffcs.buffet.controller.admin;
+
+import cn.ffcs.buffet.common.annotation.PassToken;
+import cn.ffcs.buffet.common.dto.Result;
+import cn.ffcs.buffet.model.dto.ProductCategoryDTO;
+import cn.ffcs.buffet.model.po.ProductCategoryPO;
+import cn.ffcs.buffet.service.ProductManageService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @ClassName: ProductManageController
+ * @Author: chenzc
+ * @Date: 2020/8/22
+ * @Description:
+ */
+@Api(value = "/admin/product", tags = "商品管理后台服务")
+@RestController
+@RequestMapping(value = "/admin/product")
+@Validated
+public class ProductManageController {
+
+    @Autowired
+    private ProductManageService productManageService;
+
+    @ApiOperation(value = "获取商品分类列表")
+    @GetMapping(path = "/getProductCategoryList")
+    @PassToken
+    public Result getProductCategoryList() {
+        List<ProductCategoryPO> productCategoryPOList = productManageService.getProductCategoryList();
+        return Result.success(productCategoryPOList);
+    }
+
+    @ApiOperation(value = "查询商品分类")
+    @GetMapping(path = "/productCategory/{productCategoryId}")
+    @PassToken
+    public Result updateProductCategory(@PathVariable Integer productCategoryId) {
+        ProductCategoryPO productCategoryPO = productManageService.getProductCategoryByproductCategoryId(productCategoryId);
+        return Result.success(productCategoryPO);
+    }
+
+    @ApiOperation(value = "修改商品分类")
+    @PostMapping(path = "/productCategory")
+    @PassToken
+    public Result updateProductCategory(@RequestBody ProductCategoryPO categoryPO) {
+        Integer flag = productManageService.updateProductCategory(categoryPO);
+        return Result.success(categoryPO);
+    }
+
+    @ApiOperation(value = "新建商品分类")
+    @PutMapping(path = "/productCategory")
+    @PassToken
+    public Result addProductCategory(@RequestBody ProductCategoryPO categoryPO) {
+        Integer flag = productManageService.addProductCategory(categoryPO);
+        return Result.success(categoryPO);
+    }
+
+    @ApiOperation(value = "新建商品分类")
+    @DeleteMapping(path = "/productCategory/{productCategoryId}")
+    @PassToken
+    public Result deleteProductCategory(@PathVariable Integer productCategoryId) {
+        Integer flag = productManageService.deleteProductCategory(productCategoryId);
+        return Result.success(flag);
+    }
+
+
+}
