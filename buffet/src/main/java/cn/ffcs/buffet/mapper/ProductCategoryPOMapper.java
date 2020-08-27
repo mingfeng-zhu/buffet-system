a48ebbc45a3fa040dcd1e1ff604afa67963857c3
xuefy <xuefy@ffcs.cn>
2020-08-27 10:45:02 +0800

    接口方法缺漏补充

--- buffet/src/main/java/cn/ffcs/buffet/mapper/ProductCategoryPOMapper.java
+++ buffet/src/main/java/cn/ffcs/buffet/mapper/ProductCategoryPOMapper.java
@@ -26,4 +26,6 @@ public interface ProductCategoryPOMapper {
     int updateByPrimaryKey(ProductCategoryPO record);
 
     List<ProductCategoryDTO> selectProductCategoryList();
+
+    List<ProductCategoryPO> getProductCategoryList();
 }
